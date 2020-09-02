e86fece5d9c194efc1c6b3b97d1a12731bbcb935
Lex <domian844@gmail.com>
2020-09-02 15:29:38 +0700

    wiki

--- js/main.js
+++ js/main.js
@@ -413,3 +413,32 @@ $(function () {
 }); 
 
 
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tabcontent2, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent-wiki");
+
+  tabcontent2 = document.getElementsByClassName("tabcontent-wiki2");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+    for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
